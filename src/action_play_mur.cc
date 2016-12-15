e1196a7ce77bc6a707e831ab7ad5b316bef647c9
toogy <toogy@too.gy>
2016-12-15 12:14:14 +0100

    action_play_mur

--- src/action_play_mur.cc
+++ src/action_play_mur.cc
@@ -2,11 +2,11 @@
 
 int ActionPlayMur::check(const GameState* st) const
 {
-    // FIXME
-    return 0;
+  if(amount_ < 0)
+    return INVALID_ARGUMENT;
 }
 
 void ActionPlayMur::apply_on(GameState* st) const
 {
-    // FIXME
+  // FIXME
 }
--- src/action_play_mur.hh
+++ src/action_play_mur.hh
@@ -10,29 +10,27 @@
 class ActionPlayMur : public rules::Action<GameState>
 {
 public:
-    ActionPlayMur(mur_position position, int amount, int player_id)
-        : position_(position)
-        , amount_(amount)
-        , player_id_(player_id) {}
-    ActionPlayMur() {} // for register_action()
+  ActionPlayMur(mur_position position, int amount, int player_id)
+    : position_(position), amount_(amount), player_id_(player_id) {}
+  ActionPlayMur() {} // for register_action()
 
-    int check(const GameState* st) const override;
-    void apply_on(GameState* st) const override;
+  int check(const GameState* st) const override;
+  void apply_on(GameState* st) const override;
 
-    void handle_buffer(utils::Buffer& buf) override
-    {
-        buf.handle(position_);
-        buf.handle(amount_);
-        buf.handle(player_id_);
-    }
+  void handle_buffer(utils::Buffer& buf) override
+  {
+    buf.handle(position_);
+    buf.handle(amount_);
+    buf.handle(player_id_);
+  }
 
-    uint32_t player_id() const override { return player_id_; };
-    uint32_t id() const override { return ID_ACTION_PLAY_MUR; }
+  uint32_t player_id() const override { return player_id_; };
+  uint32_t id() const override { return ID_ACTION_PLAY_MUR; }
 
 private:
-    mur_position position_;
-    int amount_;
-    int player_id_;
+  mur_position position_;
+  int amount_;
+  int player_id_;
 };
 
 #endif // !ACTION_PLAY_MUR_HH
