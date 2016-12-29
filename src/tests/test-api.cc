2af038c86ae82913b8791d5438b3823b0486bd83
toogy <toogy@too.gy>
2016-12-29 20:32:07 +0100

    [TEST] Api_MUR_ComputeStockLoss

--- src/tests/test-api.cc
+++ src/tests/test-api.cc
@@ -15,3 +15,15 @@ TEST_F(ApiTest, Api_MUR_RoleOfPlayer) {
   EXPECT_EQ(DEFENDER, players[1].api->mur_role_of_player(PLAYER_ID_1));
   EXPECT_EQ(ATTACKER, players[1].api->mur_role_of_player(PLAYER_ID_2));
 }
+
+TEST_F(ApiTest, Api_MUR_ComputeStockLoss) {
+  int stock_loss;
+  stock_loss = players[0].api->mur_compute_stock_loss(POS_N, POS_N, 42, 57);
+  EXPECT_EQ(stock_loss, 0);
+  stock_loss = players[1].api->mur_compute_stock_loss(POS_N, POS_N, 57, 42);
+  EXPECT_EQ(stock_loss, 2 * (57 + 42));
+  stock_loss = players[0].api->mur_compute_stock_loss(POS_N, POS_O, 42, 57);
+  EXPECT_EQ(stock_loss, 42);
+  stock_loss = players[1].api->mur_compute_stock_loss(POS_N, POS_O, 57, 42);
+  EXPECT_EQ(stock_loss, 42);
+}
